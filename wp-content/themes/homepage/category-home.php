b008f713304aa8a2012a52d50534f03ec07960c2
Alkagar <alkagar@gmail.com>
2012-09-04 06:25:11 +0200

    exluce home and about from loop

--- wp-content/themes/homepage/category-home.php
+++ wp-content/themes/homepage/category-home.php
@@ -6,16 +6,20 @@
     </div>
     <div class='post-content-static'>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php echo the_content(); ?>
+        <?php echo the_content(); ?>
         <?php endwhile; else: ?>
-            <p><?php _e('Sorry, no posts yet.'); ?></p>
+        <p><?php _e('Sorry, no posts yet.'); ?></p>
         <?php endif; ?>
         <h2> recent posts </h2>
-        <?php 
-            query_posts( 'cat=-9,-5' );
+        <?php
+            $slug = 'home';
+            $categoryHome = get_category_by_slug($slug);
+            $slug = 'about';
+            $categoryAbout = get_category_by_slug($slug);
+            query_posts($query_string . '&cat=-' . $categoryHome->cat_ID . ',-' . $categoryAbout->cat_ID);
             $i = 0; if ( have_posts() ) : while ( have_posts() && $i++ < 4) : the_post(); 
         ?>
-            <?php include('post-box.php'); ?>
+        <?php include('post-box.php'); ?>
         <?php endwhile; else: ?>
         <p><?php _e('Sorry, no posts yet.'); ?></p>
         <?php endif; ?>
--- wp-content/themes/homepage/index.php
+++ wp-content/themes/homepage/index.php
@@ -2,6 +2,13 @@
 <div class='home-header'>
     <h1>programmers blog <span class='size80p'>about it adventures, photo and others...</span></h1>
 </div>
+<?php
+    $slug = 'home';
+    $categoryHome = get_category_by_slug($slug);
+    $slug = 'about';
+    $categoryAbout = get_category_by_slug($slug);
+    query_posts($query_string . '&cat=-' . $categoryHome->cat_ID . ',-' . $categoryAbout->cat_ID);
+?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php include('post-box.php'); ?>
 <?php endwhile; else: ?>
