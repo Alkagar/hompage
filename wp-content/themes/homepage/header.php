950d7b5e30c58e1b1ac5bfa1011e71134c34435b
Alkagar <alkagar@gmail.com>
2012-09-04 07:23:17 +0200

    change category looks

--- wp-content/themes/homepage/header.php
+++ wp-content/themes/homepage/header.php
@@ -1,49 +1,48 @@
 <!doctype html>
 <html>
-    <head>
-        <title>hompejdż</title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <meta name="author" content="Jakub Alkagar Mrowiec - jakub@mrowiec.org" />
-        <meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.6.0/build/cssreset/cssreset-min.css">
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-        <script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery.colorbox.js"></script>
-        <script src="<?php bloginfo('stylesheet_directory'); ?>/js/hover-photo.js"></script>
-        <link href='http://fonts.googleapis.com/css?family=Spinnaker' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/blog.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/colorbox.css" type="text/css" media="screen" />
-    </head>
-    <body>
-        <div class='page'>
-            <div class='sidebar'>
-                <div class='sidebar-logo'>
-                    <a href='http://mrowiec.org' class='logo'></a>
-                </div>
-                <div class='navigation'>
-                    <ul class='menu-page'>
-                       <li><a href='/home'>home</a></li>
-                        <?php
-                            $menu = array('it' => 'blog', 'photo' => 'photo', 'tea' => 'tea');
-                            foreach($menu as $name => $link) {
-                                $categoryId = get_cat_ID($name);
-                                $categoryLink = get_category_link($categoryId);
-                            ?>
-                            <li><a href='<?php echo $categoryLink; ?>'><?php echo $link; ?></a></li>
-                            <?php
-                            }
-                        ?>
-                        <li><a href='/about'>about</a></li>
-                    </ul>
-                    <ul class='menu-social'>
-                        <li><a href=''>F</a></li>
-                        <li><a href=''>GH</a></li>
-                        <li><a href=''>NK</a></li>
-                        <li><a href=''>@</a></li>
-                    </ul>
-                </div>
-                <div class='footer'>
-                    <a href='<?php echo wp_login_url(); ?>' class='login'></a>
-                </div>
+   <head>
+      <title>hompejdż</title>
+      <meta name="description" content="" />
+      <meta name="keywords" content="" />
+      <meta name="author" content="Jakub Alkagar Mrowiec - jakub@mrowiec.org" />
+      <meta charset="utf-8" />
+      <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.6.0/build/cssreset/cssreset-min.css">
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+      <script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery.colorbox.js"></script>
+      <script src="<?php bloginfo('stylesheet_directory'); ?>/js/hover-photo.js"></script>
+      <link href='http://fonts.googleapis.com/css?family=Spinnaker' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/blog.css" type="text/css" media="screen" />
+      <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/colorbox.css" type="text/css" media="screen" />
+   </head>
+   <body>
+      <div class='page'>
+         <div class='sidebar'>
+            <div class='sidebar-logo'>
+               <a href='http://mrowiec.org' class='logo'></a>
             </div>
-            <div class='content'>
+            <div class='navigation'>
+               <ul class='menu-page'>
+                  <li><a href='/home'>home</a></li>
+                  <?php
+                     $menu = array('it' => 'blog', 'photo' => 'photo', 'tea' => 'tea', 'portfolio' => 'portfolio');
+                     foreach($menu as $name => $link) {
+                        $categoryId = get_cat_ID($name);
+                        $categoryLink = get_category_link($categoryId);
+                     ?>
+                     <li><a href='<?php echo $categoryLink; ?>'><?php echo $link; ?></a></li>
+                     <?php
+                     }
+                  ?>
+                  <li><a href='/about'>about</a></li>
+               </ul>
+               <ul class='menu-social'>
+                  <li><a href='https://www.facebook.com/jakub.mrowiec.5'>F</a></li>
+                  <li><a href='https://github.com/alkagar'>GH</a></li>
+                  <li><a href='mailto:jakub [at] mrowiec.org'>@</a></li>
+               </ul>
+            </div>
+            <div class='footer'>
+               <a href='<?php echo wp_login_url(); ?>' class='login'></a>
+            </div>
+         </div>
+         <div class='content'>
--- wp-content/themes/homepage/single.php
+++ wp-content/themes/homepage/single.php
@@ -1,16 +1,18 @@
 <?php 
-    get_header(); 
-    global $wp_query;
-    $wp_query->in_the_loop = true;
+   get_header(); 
+   if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
-                <div class='post'>
-                    <div class='post-header'>
-                        <div class='post-date'><?php the_time('j F Y'); ?></div>
-                        <div class='post-tags'><?php the_tags(''); ?></div>
-                        <h1><?php the_title(); ?></h1>
-                    </div>
-                    <div class='post-content'>
-                        <?php echo the_content(); ?>
-                    </div>
-                </div>
+<div class='post'>
+   <div class='post-header'>
+      <div class='post-date'><?php the_time('j F Y'); ?></div>
+      <div class='post-tags'><?php the_tags(''); ?></div>
+      <h1><?php the_title(); ?></h1>
+   </div>
+   <div class='post-content'>
+      <?php echo the_content(); ?>
+   </div>
+</div>
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts yet.'); ?></p>
+<?php endif; ?>
 <?php get_footer(); ?>
