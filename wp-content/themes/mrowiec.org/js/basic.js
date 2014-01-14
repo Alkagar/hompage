45e83449e4d7c06b21a6796311bf9e3be6ac51f8
Alkagar <alkagar@gmail.com>
2014-01-14 03:06:10 +0100

    refactor basic javascript code

--- wp-content/themes/mrowiec.org/js/basic.js
+++ wp-content/themes/mrowiec.org/js/basic.js
@@ -1,48 +1,60 @@
 jQuery(document).ready(function($) {
+    configureThumbnails();
     configureIsotope();
+    configureColorbox();
 
-
-    $('.post-thumbnail').mouseover(function() {
-        $(this).css({
-            'background-image' : 'url(' + $(this).data('color') + ')'
+    function configureColorbox() {
+        // show colorbox on portfolio pages with images gallery
+        $('.single-portfolio .images a.image').colorbox({
+            'maxWidth' : '90%',
+            'maxHeight' : '90%',
+            'rel' : 'portfolio-images'
         });
-    });
-    $('.post-thumbnail').mouseout(function() {
-        $(this).css({
-            'background-image' : 'url(' + $(this).data('black') + ')'
+
+        // show colorbox on listing pages when click on photo post type
+        $('.post-type-photo .post-thumbnail').click(function() {
+            // get long description from photo post thumbnail
+            var longDescription = $(this).parent().parent().find('.post-long-description').html();
+            // triger colorbox
+            $.colorbox({
+                href: $(this).data('image'), 
+                'maxWidth' : '90%', 
+                'maxHeight' : '90%', 
+                'title' : function() { 
+                    return longDescription;
+                }
+            });
         });
-    });
+    }
 
-    $('.post-thumbnail').mouseover(function() {
-        var metaDate = $(this).parent().parent().find('.post-meta .post-date');
-        $(this).find('.post-title').append(metaDate);
-    });
-    $('.post-thumbnail').mouseout(function() {
-        var metaDate = $(this).find('.post-date');
-        var meta = $(this).parent().parent().find('.post-meta');
-        meta.append(metaDate);
-    });
-
-    $('.post-type-photo .post-thumbnail').click(function() {
-        var longDescription = $(this).parent().parent().find('.post-long-description').html();
-        $.colorbox({
-            href: $(this).data('image'), 
-            'maxWidth' : '90%', 
-            'maxHeight' : '90%', 
-            'title' : function() { 
-                return longDescription;
-            }
+    function configureThumbnails() {
+        // prevent default link action from photo type thumbnails
+        $('.post-type-photo a.permalink').click(function(e) {
+            e.preventDefault();
         });
-    });
-    $('.post-type-photo a.permalink').click(function(e) {
-        e.preventDefault();
-    });
 
-    $('.single-portfolio .images a.image').colorbox({
-        'maxWidth' : '90%',
-        'maxHeight' : '90%',
-        'rel' : 'portfolio-images'
-    });
+        $('.post-thumbnail').mouseover(function() {
+            // set background image to color image
+            $(this).css({
+                'background-image' : 'url(' + $(this).data('color') + ')'
+            });
+
+            // add post date to post box
+            var metaDate = $(this).parent().parent().find('.post-meta .post-date');
+            $(this).find('.post-title').append(metaDate);
+        });
+        $('.post-thumbnail').mouseout(function() {
+            // revert back to b/w background image
+            $(this).css({
+                'background-image' : 'url(' + $(this).data('black') + ')'
+            });
+
+            // remove post date from post box
+            var metaDate = $(this).find('.post-date');
+            var meta = $(this).parent().parent().find('.post-meta');
+            meta.append(metaDate);
+        });
+    }
 
     function configureIsotope() {
         var container = $('.isotope');
