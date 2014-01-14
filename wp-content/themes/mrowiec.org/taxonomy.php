84e8e37569207fc8efd0e37fd147bc8bdce7cb51
Alkagar <alkagar@gmail.com>
2014-01-14 01:40:17 +0100

    display taxonomy name as heading

--- wp-content/themes/mrowiec.org/taxonomy.php
+++ wp-content/themes/mrowiec.org/taxonomy.php
@@ -1,7 +1,12 @@
-<?php get_header(); ?>
+<?php 
+    get_header(); 
+    $taxonomy = get_taxonomy( get_query_var( 'taxonomy' ) );
+    $term = get_query_var( 'term');
+?>
+<header>
+<h2> posted in: <u><?php echo $term; ?></u> </h2>
+</header>
 <div class="content isotope">
-    <?php 
-    ?>
     <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
     <?php get_template_part('content', 'postbox'); ?>
     <?php endwhile; endif; ?>
